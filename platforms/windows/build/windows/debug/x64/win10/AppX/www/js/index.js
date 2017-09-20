203de38edf7ceba0aefd2c9dee15dc44dfe9e2f0
Lukas Schrader <Lukas.Schrader@point-gmbh.com>
2017-09-20 10:57:53 +0200

    init
    asd

---
+++ platforms/windows/build/windows/debug/x64/win10/AppX/www/js/index.js
@@ -0,0 +1,144 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready')
+
+        window.open = cordova.InAppBrowser.open;
+        var ref = cordova.InAppBrowser.open('ms-appdata:///local//index.html', '_blank', 'fullscreen=yes');
+        
+        ref.addEventListener('myEvent', openCamera());
+        //TakePicture
+
+        function loadStopHandler() {
+            console.log('in cordova');
+            // sending a JS injection string to fetch a sessionStorage element
+            app.subWindow.executeScript(
+                { code: "alert('message');" },
+                function(data) {  // callback function
+                    if (data) {
+                        // you've passed a data from InAppBrowser to Cordova app
+                        alert('with data: ');
+                    }
+                }
+            );
+        }
+
+        function openCamera() {
+            console.log('in openCam');
+            var srcType = Camera.PictureSourceType.CAMERA;
+            var options = setOptions(srcType);
+            var func = createNewFileEntry;
+
+            navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+                displayImage(imageUri);
+                // You may choose to copy the picture, save it somewhere, or upload.
+                func(imageUri);
+
+            }, function cameraError(error) {
+                console.debug("Unable to obtain picture: " + error, "app");
+
+            }, options);
+        }
+
+        function setOptions(srcType) {
+            var options = {
+                // Some common settings are 20, 50, and 100
+                quality: 50,
+                destinationType: Camera.DestinationType.FILE_URI,
+                // In this app, dynamically set the picture source, Camera or photo gallery
+                sourceType: srcType,
+                encodingType: Camera.EncodingType.JPEG,
+                mediaType: Camera.MediaType.PICTURE,
+                allowEdit: true,
+                correctOrientation: true  //Corrects Android orientation quirks
+            };
+            return options;
+        }
+        function createNewFileEntry(imgUri) {
+            window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, function success(dirEntry) {
+
+                // JPEG file
+                dirEntry.getFile("tempFile.jpeg", { create: true, exclusive: false }, function (fileEntry) {
+
+                    // Do something with it, like write to it, upload it, etc.
+                    // writeFile(fileEntry, imgUri);
+                    console.log("got file: " + fileEntry.fullPath);
+                    // displayFileData(fileEntry.fullPath, "File copied to");
+
+                }, onErrorCreateFile);
+
+            }, onErrorResolveUrl);
+        }
+
+        document.getElementById('openBarcodeScanner').addEventListener('click', openScanner, false);
+        function openScanner() {
+            console.log('in scanner function');
+            cordova.plugins.barcodeScanner.scan(
+                function (result) {
+                    if (!result.cancelled) {
+                        if (result.format === "QR_CODE") {
+                            navigator.notification.prompt("Please enter name of data", function (input) {
+                                var name = input.input1;
+                                var value = result.text;
+
+                                var data = localStorage.getItem("LocalData");
+                                console.log(data);
+                                data = JSON.parse(data);
+                                data[data.length] = [name, value];
+
+                                localStorage.setItem("LocalData", JSON.stringify(data));
+
+                                alert("Done");
+                            });
+                        }
+                    }
+                },
+                function (error) {
+                    alert("Scanning failed: " + error);
+                }
+            );
+        }
+      
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
---
+++ platforms/windows/platform_www/cordova_plugins.js
@@ -0,0 +1,96 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "id": "cordova-sqlite-storage.SQLiteProxy",
+        "file": "plugins/cordova-sqlite-storage/src/windows/sqlite-proxy.js",
+        "pluginId": "cordova-sqlite-storage",
+        "runs": true
+    },
+    {
+        "id": "cordova-sqlite-storage.SQLite3",
+        "file": "plugins/cordova-sqlite-storage/src/windows/SQLite3-Win-RT/SQLite3JS/js/SQLite3.js",
+        "pluginId": "cordova-sqlite-storage",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "file": "plugins/cordova-plugin-inappbrowser/src/windows/InAppBrowserProxy.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-camera.Camera",
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.camera",
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.CameraPopoverHandle",
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.CameraProxy",
+        "file": "plugins/cordova-plugin-camera/src/windows/CameraProxy.js",
+        "pluginId": "cordova-plugin-camera",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/cordova-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "cordova-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-sqlite-storage": "2.0.2",
+    "cordova-plugin-inappbrowser": "1.7.1",
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-camera": "2.4.1",
+    "cordova-plugin-barcodescanner": "0.7.2"
+};
+// BOTTOM OF METADATA
+});
